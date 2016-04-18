21add918679475c7171af6bda93139c9b71d0f2d
Anthony Howe <achowe@snert.com>
2016-04-18 16:32:42 -0400

    Modify addpasswd.sh to be able to delete user or script a password change.

--- scripts/addpasswd.sh
+++ scripts/addpasswd.sh
@@ -3,29 +3,55 @@
 # Add/change a password in an Nginx password file.
 #
 
+function usage
+{
+        echo 'usage: addpasswd.sh [-d][-p password] file user'
+        exit 2
+}
+
+__change=true
+
+args=$(getopt 'dp:' $*)
+if [ $? -ne 0 ]; then
+	usage
+fi
+set -- $args
+while [ $# -gt 0 ]; do
+        case "$1" in
+        (-d) __change=false ;;
+	(-p) pass1=$2; shift ;;
+        (--) shift; break ;;
+        esac
+        shift
+done
 if [ $# -ne 2 ]; then
-	echo 'usage: addpasswd.sh file user'
-	exit 1
+	usage
 fi
 
 __file=$1
 __user=$2
 
-stty -echo
-pass1=X
-while [ "$pass1" != "$pass2" ]; do
-	printf 'Password: '
-	read -r pass1
-	echo
-	printf 'Repeat password: '
-	read -r pass2
-	echo
-done
-stty echo
+if $__change && [ -z "$pass1" ]; then
+	stty -echo
+	pass1=X
+	while [ "$pass1" != "$pass2" ]; do
+		printf 'Password: '
+		read -r pass1
+		echo
+		printf 'Repeat password: '
+		read -r pass2
+		echo
+	done
+	stty echo
+fi
 
 if [ -f "$__file" ]; then
+	# Delete an existing user.
 	sed -e"/^$__user:/d" $__file >$$.tmp
 	mv $$.tmp $__file
 fi
-printf "$__user:$(openssl passwd -1 $pass1)\n" >>$__file
-
+if $__change; then
+	printf "$__user:$(openssl passwd -1 $pass1)\n" >>$__file
+	sort $__file >$$.tmp
+	mv $$.tmp $__file
+fi
