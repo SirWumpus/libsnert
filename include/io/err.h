e7a45bfaa34d45d3579aa853b4a7833ab1b8075a
Anthony Howe <achowe@snert.com>
2022-02-17 09:29:24 -0500

    Fix io/err.h to cast ignored function returns.

--- include/io/err.h
+++ include/io/err.h
@@ -12,34 +12,49 @@ extern "C" {
 #include <errno.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 #define warn(...)	warnc(errno, __VA_ARGS__)
-#define warnx(...)	{ fprintf(stderr, __VA_ARGS__); fputc('\n', stderr); }
+#define warnx(...)	{ \
+	(void) fprintf(stderr, __VA_ARGS__); \
+	(void) fputc('\n', stderr); \
+}
 #define warnc(code, ...)	{ \
-	fprintf(stderr, __VA_ARGS__); \
-	fprintf(stderr, ": %s\n", strerror(code)); \
+	(void) fprintf(stderr, __VA_ARGS__); \
+	(void) fprintf(stderr, ": %s\n", strerror(code)); \
 }
 
 #define vwarn(fmt, args)  vwarnc(errno, fmt, args)
-#define vwarnx(fmt, args) { vfprintf(stderr, fmt, args); fputc('\n', stderr); }
+#define vwarnx(fmt, args) { \
+	(void) vfprintf(stderr, fmt, args); \
+	(void) fputc('\n', stderr); \
+}
 #define vwarnc(code, fmt, args)  { \
-	vfprintf(stderr, fmt, args); \
-	fprintf(stderr, "%s%s\n", (fmt) ? ": " : "", strerror(code)); \
+	(void) vfprintf(stderr, fmt, args); \
+	(void) fprintf(stderr, "%s%s\n", (fmt) ? ": " : "", strerror(code)); \
 }
 
 #define err(ex, ...)  errc(ex, errno, __VA_ARGS__)
-#define errx(ex, ...) { fprintf(stderr, __VA_ARGS__); fputc('\n', stderr); exit(ex); }
+#define errx(ex, ...) { \
+	(void) fprintf(stderr, __VA_ARGS__); \
+	(void) fputc('\n', stderr); \
+	exit(ex); \
+}
 #define errc(ex, code, ...)  { \
-	fprintf(stderr, __VA_ARGS__); \
-	fprintf(stderr, ": %s\n", strerror(code)); \
+	(void) fprintf(stderr, __VA_ARGS__); \
+	(void) fprintf(stderr, ": %s\n", strerror(code)); \
 	exit(ex); \
 }
 
 #define verr(ex, fmt, args)  verrc(ex, errno, fmt args)
-#define verrx(ex, fmt, args) { vfprintf(stderr, fmt, args); fputc('\n', stderr); exit(ex); }
+#define verrx(ex, fmt, args) { \
+	(void) vfprintf(stderr, fmt, args); \
+	(void) fputc('\n', stderr); \
+	exit(ex); \
+}
 #define verrc(ex, code, fmt, args) { \
-	vfprintf(stderr, fmt, args); \
-	fprintf(stderr, "%s%s\n", (fmt) ? ": " : "", strerror(code)); \
+	(void) vfprintf(stderr, fmt, args); \
+	(void) fprintf(stderr, "%s%s\n", (fmt) ? ": " : "", strerror(code)); \
 	exit(ex); \
 }
 
